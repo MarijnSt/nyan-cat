6ed8a781e26416cf7b6515442efb9a8ae4238643
MarijnSt <marijn.stammeleer@gmail.com>
2019-03-27 12:16:20 +0100

    one cat, random start

--- README.md
+++ README.md
@@ -23,4 +23,5 @@ Properties to randomize
 Resources
 https://developer.mozilla.org/en-US/docs/Web/CSS/transition
 https://developer.mozilla.org/en-US/docs/Web/CSS/transform
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 
--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,14 @@ var knopke = document.querySelector("button");
 var kat = document.querySelector("#kat");
 
 knopke.addEventListener("click", function(){
-	kat.classList.toggle("hidden");
 	kat.classList.toggle("nyan-start");
+	if(kat.classList.contains("nyan-start")){
+		kat.style.left = randomGetalX();
+	}
+	kat.classList.toggle("hidden");
 });
+
+function randomGetalX(){
+	var random = Math.floor(Math.random() * 100) + "vw";
+	return random;
+}
