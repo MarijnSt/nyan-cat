edadb6c0e91df6bd3b4727ecc8f329a2d4d69eca
MarijnSt <marijn.stammeleer@gmail.com>
2019-03-27 16:07:34 +0100

    one click = one random cat

--- README.md
+++ README.md
@@ -2,21 +2,21 @@
 
 Process
 
-	- 	Create one cat with animation
-		-	Add start and endposition in a class
-		-	Transition property to move from one state to the other
+	* 	Create one cat with animation
+		*	Add start and endposition in a class
+		*	Transition property to move from one state to the other
+	*	Randomize one cat (random start position and an end position that's a bit to the right of the start
 
 Next step
 
-	-	Second click resets everything
-	-	Randomize one cat
+	*	Second click resets everything
+	*	Randomize one cat
 
 Properties to randomize
 
-	-	top (start en end)
-	-	left (start en end)
-	-	transform rotate (end)
-	-	transition (element)
+	*	left (start and end)
+	*	transform rotate (end)?
+	*	transition (element)
 
 
 
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,23 @@
 var knopke = document.querySelector("button");
 var kat = document.querySelector("#kat");
 
+var posX = 0;
+
+function randomPositieTop(i){
+	posX = Math.floor(Math.random() * 100);
+	var random = posX + "vw";
+	i.style.left = random;
+}
+
 knopke.addEventListener("click", function(){
-	kat.classList.toggle("nyan-start");
-	if(kat.classList.contains("nyan-start")){
-		kat.style.left = randomGetalX();
-	}
+	kat.classList.add("nyan-start");
+	randomPositieTop(kat);
 	kat.classList.toggle("hidden");
+	setTimeout(function(){
+		kat.classList.remove("nyan-start");
+		kat.classList.toggle("nyan-end");
+		var posBottom = (posX + 20) + "vw";
+		kat.style.left = posBottom;
+	}, 1);
 });
 
-function randomGetalX(){
-	var random = Math.floor(Math.random() * 100) + "vw";
-	return random;
-}
