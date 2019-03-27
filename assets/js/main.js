2f19b765149bd2c634c36fec80ee843d35de6a1a
MarijnSt <marijn.stammeleer@gmail.com>
2019-03-27 11:59:46 +0100

    one cat, starts hidden

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,6 @@ var knopke = document.querySelector("button");
 var kat = document.querySelector("#kat");
 
 knopke.addEventListener("click", function(){
+	kat.classList.toggle("hidden");
 	kat.classList.toggle("nyan-start");
-	kat.classList.toggle("nyan-end");
 });
